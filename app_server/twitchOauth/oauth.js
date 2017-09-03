6942e06a16ee2c23c0418077e68aab83b83beb24
Joe.Kalb <kalb.joseph@gmail.com>
2017-09-02 21:44:11 -0700

    back to local testing

--- app_server/twitchOauth/oauth.js
+++ app_server/twitchOauth/oauth.js
@@ -3,6 +3,7 @@ const { CONFIG } = require('./config');
 
 
 module.exports.grabTwitchTokens = function(req, res, next){
+  console.log(req.params.code);
   let call = 'https://api.twitch.tv/kraken/oauth2/token?client_id={client_ID}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={redirect}';
   call = call.replace('{client_ID}', CONFIG.client_id)
       .replace('{client_secret}', CONFIG.client_secret)
