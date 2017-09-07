8c0eec64f33b4b36742f9fdcace0196bb5a87ee2
JoeKalb <kalb.joseph@gmail.com>
2017-09-07 15:56:09 -0700

    aws

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,59 +1,9 @@
-const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
-let gracefulShutdown;
-
-const dbUri = 'mongodb://localhost:27017/emoterv1';
-
-Grid.mongo = mongoose.mongo;
-mongoose.connect(dbUri, {
-	useMongoClient: true
-});
-
-
-// add grid-fs to mongoose
-mongoose.connection.once('useMongoClient', function() {
-	let gfs = Grid(mongoose.connection.db);
-});
-// Connection events
-mongoose.connection.on('connected', function() {
-	console.log('Mongoose connected to ' + dbUri);
-});
-mongoose.connection.on('error', function(err) {
-	console.log('Mongoose connection error: ' + err);
-});
-mongoose.connection.on('disconnected', function() {
-	console.log('Mongoose disconnected');
-});
-
-
-
-// capture app termination and restart the server
-gracefulShutdown = function(msg, callback) {
-	mongoose.connection.close(function() {
-		console.log('Mongoose disconnected through ' + msg);
-		callback();
-	});
-};
-// nodemon restars
-process.once('SIGUSR2', function() {
-	gracefulShutdown('nodemon restart', function() {
-		process.kill(process.pid, 'SIGUSR2');
-	});
-});
-// for app termination
-process.on('SIGINT', function() {
-	gracefulShutdown('app termination', function() {
-		process.exit(0);
-	});
-});
-// for heroku app termination
-process.on('SIGTERM', function() {
-	gracefulShutdown('Heroku app termination', function(){
-		process.exit(0);
-	});
-});
-
-// bring in models
-require('./art');
-require('./commission');
-require('./user');
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB();
+/*
+dynamodb.getItem(params, function(err, data) {
+                     if (data) {
+                        doSomethingWithItem(data.Item);
+                     }
+                 });
+*/
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const passport = require('passport');
 
-//require('./app_server/models/db');
+require('./app_server/models/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
